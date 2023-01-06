10dd319a8c16173ab22ff11bd10cb622565ebc47
Pranith Raj <pranithraj77@gmail.com>
2023-01-06 16:39:07 -0500

    Update SetMatrixZeroes.java
    Added an easier implementation

--- SetMatrixZeroes.java
+++ SetMatrixZeroes.java
@@ -44,4 +44,49 @@ public class SetMatrixZeroes {
 
   System.out.println(Arrays.toString(matrix));
  }
+ 
+ // Easier to understand
+ public void setZeroes2(int[][] matrix) {
+        int cols = matrix[0].length - 1, rows = matrix.length - 1;
+        boolean setRow = false, setCol = false;
+        
+        if (matrix[0][0] != 0) {
+            for (int i = 1; i <= rows; i++) {
+                if (matrix[i][0] == 0) {
+                    setCol = true;
+                    matrix[0][0] = 0;
+                }
+            }
+            
+            for (int i = 1; i <= cols; i++) {
+                if (matrix[0][i] == 0) {
+                    setRow = true;
+                    matrix[0][0] = 0;
+                }
+            }
+        } else {
+            setRow = true;
+            setCol = true;
+        }
+
+        // Setting the first element of row and col to 0 if we encounter a 0
+        for (int i = rows; i > 0; i--) {
+            for (int j = cols; j > 0; j--) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        // Traverse the first row except 0th index
+        for (int i = rows; i > 0; i--) {
+            for (int j = cols; j > 0; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+        
+        if(setCol) for (int i = rows; i > 0; i--) matrix[i][0] = 0;
+        if(setRow) for (int i = cols; i > 0; i--) matrix[0][i] = 0;
+    }
 }
